a53f2266ea251ce2af90544f448f06c30270c858
Sidhant Sadyal <116481420+Sid0225s@users.noreply.github.com>
2023-06-07 15:21:40 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Detail from "./pages/Moviedetail/Detail";
+import Movilist from "./component/Movielist/Movilist";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Detail />}></Route>
+          <Route path="movies/:type" element={<Movilist />}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import MovieList from "../../component/Movielist/Movilist";
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+function Home() {
+  const [popularmovies, setPopularmovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
+    )
+      .then((res) => res.json())
+      .then((data) => setPopularmovies(data.results));
+  });
+  return (
+    <div>
+      <Carousel responsive={responsive}>
+        {popularmovies.map((movie) => (
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to={`/movie/${movie.id}`}
+          >
+            <div className="posterImage">
+              <img
+                src={`https://image.tmdb.org/t/p/original${
+                  movie && movie.backdrop_path
+                }`}
+                alt=""
+              />
+            </div>
+            <div className="posterImage__overlay">
+              <div className="posterImage__title">
+                {movie ? movie.original_title : ""}
+              </div>
+              <div className="posterImage__runtime">
+                {movie ? movie.release_date : ""}
+                <span className="posterImage__rating">
+                  {movie ? movie.vote_average : ""}
+                  <i className="fa fa-star" />{" "}
+                </span>
+              </div>
+              <div className="posterImage__description">
+                {movie ? movie.overview : ""}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </Carousel>
+      <MovieList />
+    </div>
+  );
+}
+
+export default Home;
